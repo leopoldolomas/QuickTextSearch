929ebe8655008d9f37cf7b9153ffaf144db973c4
Leo <leopoldolomas@gmail.com>
2015-06-03 10:03:08 -0500

    +v0.1

---
+++ QuickTextSearch/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MergeHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtToSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtContent.Location = new System.Drawing.Point(0, 55);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtContent.Size = new System.Drawing.Size(903, 503);
+            this.txtContent.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.txtToSearch);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(903, 55);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(318, 13);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(50, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtToSearch
+            // 
+            this.txtToSearch.Location = new System.Drawing.Point(96, 15);
+            this.txtToSearch.Name = "txtToSearch";
+            this.txtToSearch.Size = new System.Drawing.Size(216, 20);
+            this.txtToSearch.TabIndex = 3;
+            this.txtToSearch.TextChanged += new System.EventHandler(this.txtToSearch_TextChanged);
+            this.txtToSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtToSearch_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text to search:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 558);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quick Text Search";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtToSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ QuickTextSearch/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MergeHelper
+{
+    public partial class MainForm : Form
+    {
+        #region Properties
+        private string OriginalText { get; set; }
+        #endregion
+
+        #region Constructor(s)
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Methods
+        private void copyTextLineIntoStr(string line, ref string content)
+        {
+            content += line + Environment.NewLine;
+        }
+
+        private string searchTextInContent(string textToSearch, string content)
+        {
+            string output = String.Empty;
+
+            var lines = new List<string>(content.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
+            var result = lines.Where(i => i.ToLower().Contains(textToSearch.ToLower())).ToList();
+            result.ForEach(i => copyTextLineIntoStr(i, ref output));
+
+            return output;
+        }
+        #endregion
+
+        #region Events
+        private void txtToSearch_TextChanged(object sender, EventArgs e)
+        {
+            txtContent.Text = searchTextInContent(txtToSearch.Text, OriginalText);
+        }
+
+        private void txtToSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (String.IsNullOrEmpty((sender as TextBox).Text))
+            {
+                OriginalText = txtContent.Text;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtToSearch.Clear();
+        }
+        #endregion
+    }
+}
